ebe81a61f6afd1ea1bb62f3576f18a818467470a
David Marx <david.marx84@gmail.com>
2020-12-02 20:06:29 -0800

    regex solution was wrong, needed to count character occurences, not length of character string. solved with list comprehension.

--- day2/solution.py
+++ day2/solution.py
@@ -4,14 +4,24 @@ import re
 def parse(line):
     rule_lhs, rule_rhs, pwd= line.split(' ')
     nmin, nmax = rule_lhs.split('-')
+    nmin, nmax = int(nmin), int(nmax)
     reqd_string = rule_rhs[:-1]
     return nmin, nmax, reqd_string, pwd
     
 
 def validate(line):
     nmin, nmax, reqd_string, pwd = parse(line)
-    pat = f".*{reqd_string}{{{nmin},{nmax}}}.*"
-    return re.match(pat, pwd)
+    #pat = f".*{reqd_string}{{{nmin},{nmax}}}.*" # 357, too low
+    #pat = f"{reqd_string}{{{nmin},{nmax}}}" # 190, even lower
+    # return re.match(pat, pwd)
+    
+    # Doesn't have to be sequential, just a count
+    # currently, reqd_string is just a single character. 
+    # don't need to over engineer
+    tgts = [v for v in pwd if v == reqd_string]
+    return nmin <= len(tgts) <= nmax
+    
+    
 
 @click.command()
 @click.argument("infile", default="input.txt", type=click.File("rb"))
